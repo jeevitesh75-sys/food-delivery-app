a31ddc8483d9d8dc69cb652dc361e01be0e486eb
jeevitesh75-sys <jeevitesh75@gmail.com>
2026-06-22 11:00:49 +0530

    remove nested Food app folder

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,40 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions =
+require("firebase-functions");
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+const admin =
+require("firebase-admin");
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+exports.sendNotification =
+functions.https.onRequest(
+async(req,res)=>{
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+try{
+
+await admin.messaging().send({
+
+token:
+req.body.token,
+
+notification:{
+title:req.body.title,
+body:req.body.body
+}
+
+});
+
+res.send("sent");
+
+}catch(e){
+
+console.log(e);
+
+res.status(500).send(
+e.message
+);
+
+}
+
+}
+);
