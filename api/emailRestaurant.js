718218b4e6cd68343b0682558f879e106c4693bd
jeevitesh75-sys <jeevitesh75@gmail.com>
2026-07-24 12:13:03 +0530

    Add CORS headers to fix preflight OPTIONS failures

--- api/emailRestaurant.js
+++ api/emailRestaurant.js
@@ -1,3 +1,15 @@
+module.exports = async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  if (req.method !== "POST") return res.status(405).end();
+
+  // ... rest of existing code stays the same
 const { db } = require("./_firebaseAdmin.js");
 const { transporter } = require("./_mailer.js");
 
