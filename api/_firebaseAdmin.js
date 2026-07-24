485f45cbe4c78cf2bf4965a74b34bd5feba6580d
jeevitesh75-sys <jeevitesh75@gmail.com>
2026-07-24 11:38:48 +0530

    Use firebase-admin modular API instead of namespace API

--- api/_firebaseAdmin.js
+++ api/_firebaseAdmin.js
@@ -1,13 +1,9 @@
-const admin = require("firebase-admin");
+const { initializeApp, getApps, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
-console.log("DEBUG projectId:", process.env.FIREBASE_PROJECT_ID);
-console.log("DEBUG clientEmail:", process.env.FIREBASE_CLIENT_EMAIL);
-console.log("DEBUG privateKey exists:", !!process.env.FIREBASE_PRIVATE_KEY);
-console.log("DEBUG privateKey length:", process.env.FIREBASE_PRIVATE_KEY?.length);
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
@@ -15,5 +11,5 @@ if (!admin.apps.length) {
   });
 }
 
-const db = admin.firestore();
+const db = getFirestore();
 module.exports = { db };
